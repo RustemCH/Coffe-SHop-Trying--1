5bb5184190c82f6703fbb7be624a7f69a6114c1f
Rustem Demir <rstmdmr19@gmail.com>
2025-06-22 19:41:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-const menuBtn = document.getElementById("menu-btn");
-const navbar = document.querySelector("header nav");
 
-menuBtn.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
